32a4cc5becc01459e33da9a4865c851774f272ba
bepyan <bepyan@naver.com>
2026-07-19 02:42:36 +0900

    feat: use satori make og

---
+++ src/pages/og.png.ts
@@ -0,0 +1,77 @@
+import { readFile } from 'node:fs/promises';
+import { Resvg } from '@resvg/resvg-js';
+import type { APIRoute } from 'astro';
+import satori from 'satori';
+
+const WIDTH = 1200;
+const HEIGHT = 630;
+const SCALE = 2;
+
+const PATTERN = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23111111' fill-opacity='0.08'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
+
+const markup = {
+  type: 'div',
+  props: {
+    style: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      backgroundImage: PATTERN,
+      backgroundSize: '100px 100px',
+      backgroundRepeat: 'repeat',
+      color: '#111111',
+      fontFamily: 'Arita Dotum',
+      padding: '90px 96px',
+    },
+    children: [
+      {
+        type: 'div',
+        props: {
+          style: {
+            display: 'flex',
+            fontSize: '168px',
+            letterSpacing: '-3px',
+            lineHeight: 1,
+          },
+          children: [
+            { type: 'div', props: { style: { fontWeight: 700 }, children: '아리따' } },
+            {
+              type: 'div',
+              props: { style: { fontWeight: 100, marginLeft: '44px' }, children: '글꼴' },
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+export const GET: APIRoute = async () => {
+  const [dotumBold, dotumThin] = await Promise.all([
+    readFile('fonts/AritaDotumKR-Bold.ttf'),
+    readFile('fonts/AritaDotumKR-Thin.ttf'),
+  ]);
+
+  const svg = await satori(markup, {
+    width: WIDTH,
+    height: HEIGHT,
+    fonts: [
+      { name: 'Arita Dotum', data: dotumBold, weight: 700, style: 'normal' },
+      { name: 'Arita Dotum', data: dotumThin, weight: 100, style: 'normal' },
+    ],
+  });
+
+  const png = new Resvg(svg, {
+    fitTo: { mode: 'width', value: WIDTH * SCALE },
+  })
+    .render()
+    .asPng();
+
+  return new Response(new Uint8Array(png), {
+    headers: { 'Content-Type': 'image/png' },
+  });
+};
